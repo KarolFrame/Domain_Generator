586a63ab916c34ec9a59051186828cf6af6a630e
Carolina <carolina.peseca@gmail.com>
2025-04-23 11:31:31 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon'];
+const extension = ['.com', '.es', '.net', '.io', '.us'];
+
+function allDomains(){
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let z = 0; z < extension.length; z++) {
+          const domain = pronoun[i] + adj[x] + noun[y] + extension[z];
+          console.log(domain);
+        }
+      }
+    }
+  }
+}
+
+allDomains();
